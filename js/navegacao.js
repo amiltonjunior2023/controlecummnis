0460b0c48ed830a6858ed1aecaf5f7dc99201f9b
amiltonjunior2023 <150165827+amiltonjunior2023@users.noreply.github.com>
2024-10-17 21:37:38 -0300

    subir

---
+++ js/navegacao.js
@@ -0,0 +1,5 @@
+function redirecionar() {
+    setTimeout(function() {
+        window.location.href = "./controle.html"; // Verifique se o caminho está correto
+    }, 2000); // 3000 milissegundos = 3 segundos
+}
